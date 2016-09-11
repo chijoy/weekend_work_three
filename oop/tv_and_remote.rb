3aa883b5734de8e8f2f6007f40605d1a425cefea
Joy <joymccaffreychicago@gmail.com>
2016-09-11 17:51:03 -0500

    fix remote class.

--- oop/tv_and_remote.rb
+++ oop/tv_and_remote.rb
@@ -3,6 +3,70 @@
 
 # The Tv class will have the attributes: power, volume, and channel. 
 
+class Tv
+  attr_accessor :power, :volume, :channel
+
+  def initialize(input_options_hash)
+    @power = input_options_hash[:power]
+    @volume = input_options_hash[:volume]
+    @channel = input_options_hash[:channel]
+  end
+end
+
+class Remote
+  attr_accessor :tv
+
+  def initialize(tv_info)
+    
+    @tv = tv_info[:tv_type]
+  end
+
+  def toggle_power
+    if tv.power == true
+      false
+    elsif tv.power == false
+      true
+    end
+  end
+
+  def increment_volume
+    tv.volume += 1
+  end
+
+  def decrement_volume
+    tv.volume -= 1
+  end
+  
+  def set_channel(new_channel)
+    tv.channel = new_channel
+  end
+    
+end
+
+# DRIVER CODE:
+
+tv1 = Tv.new(power: true, volume: 1, channel: 2)
+puts "Initializing the tv" 
+p tv1
+puts "Testing whether the tv power is on."
+p tv1.power
+puts "Testing the tv volume."
+p tv1.volume
+puts "Testing the tv channel."
+p tv1.channel
+
+remote = Remote.new(tv_type: tv1)
+puts "Testing whether the remoteis on."
+p remote
+remote.toggle_power
+p tv1.power
+remote.increment_volume
+remote.increment_volume
+p tv1.volume
+
+remote.set_channel(4)
+p tv1.channel
+
 # The Remote class will have just one attribute: tv. This will represent which tv it
 # actually controls.
 # The Remote will have the following instance methods: 
